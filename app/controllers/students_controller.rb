a5b35eaec4ff41dd8cf8bc6b39620096eb20f143
Jonathan Wong <jonathan.wong110@gmail.com>
2019-09-01 06:33:46 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -19,7 +19,7 @@ class StudentsController < ApplicationController
    
   def update
     @student = Student.find(params[:id])
-    @student.update(student_params(:first_name))
+    @student.update(student_params(:first_name, :last_name))
     redirect_to student_path(@student)
   end
 
