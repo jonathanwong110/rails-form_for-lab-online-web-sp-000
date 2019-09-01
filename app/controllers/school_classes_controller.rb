47bfdca214194e80af9f0f3ed20d7f7842d51419
Jonathan Wong <jonathan.wong110@gmail.com>
2019-09-01 06:23:27 +0000

    Automatically backed up by Learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -12,9 +12,9 @@ class SchoolClassesController < ApplicationController
 	end
 
   def create
-    @school_class = SchoolClass.new(student_params(:title, :room_number))
+    @school_class = SchoolClass.new(school_class_params(:title, :room_number))
     @school_class.save
-    redirect_to student_path(@school_class)
+    redirect_to school_class_path(@school_class)
   end
    
   def update
